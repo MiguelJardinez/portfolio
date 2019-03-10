baa77faf50942925fa98572170a232c197e730a1
Miguel <jardinez7514@hotmail.com>
2019-03-10 13:33:34 -0600

    Se termina el boton hamburguesa y el portafolio profesional responsive

--- css/index.js
+++ css/index.js
@@ -3,3 +3,22 @@ const menu = document.querySelector('.menu');
 
 const burgerButton = document.querySelector('#burger-menu')
 console.log(burgerButton);
+
+ipad.addListener(validation)
+
+function validation (event){
+    if (ipad.matches){
+        burgerButton.addEventListener('click', hideshow)
+    } else{
+        burgerButton.removeEventListener('click', hideshow)
+    }
+}
+validation(ipad);
+
+function hideshow(){
+    if(menu.classList.contains('is-active')){
+        menu.classList.remove('is-active');
+    }else{
+        menu.classList.add('is-active');
+    }
+}
