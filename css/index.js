ec112cbb729a7a870c0a41212d2e0ea94ca4ab56
Miguel <jardinez7514@hotmail.com>
2019-03-10 13:24:38 -0600

    Se termina de darle estilos al menu responsive y se agrego el archivo JS para activarlo

---
+++ css/index.js
@@ -0,0 +1,5 @@
+const ipad = window.matchMedia ('screen and (max-width: 767px)');
+const menu = document.querySelector('.menu');
+
+const burgerButton = document.querySelector('#burger-menu')
+console.log(burgerButton);
